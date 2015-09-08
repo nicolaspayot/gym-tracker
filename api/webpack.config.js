2462b41f11c964fe896143acdc9567e66c4ffb76
Nicolas Payot <nicolas.payot@gmail.com>
2015-09-08 22:55:24 +0200

    chore(webpack): prevent webpack to bundle node_modules

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -1,10 +1,19 @@
 var path = require('path');
+var fs = require('fs');
+
+var nodeModules = fs
+  .readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  });
 
 module.exports = {
   entry: './src/app.js',
   target: 'node',
   output: {
     path: path.join(__dirname, 'build'),
-    filename: 'backend.js'
-  }
+    filename: 'backend.js',
+    libraryTarget: 'commonjs2'
+  },
+  externals: nodeModules
 }
