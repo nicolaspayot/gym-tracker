401741bfa1fe2a809772b33f6336509ae0f39c4a
Nicolas Payot <nicolas.payot@gmail.com>
2015-09-24 15:02:50 +0200

    chore(eslint): add eslint

--- api/src/workout.js
+++ api/src/workout.js
@@ -1,5 +1,5 @@
 export default function workout(req, res) {
-  var body = 'Hello, World!';
+  const body = 'Hello, World!';
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', Buffer.byteLength(body));
   res.end(body);
