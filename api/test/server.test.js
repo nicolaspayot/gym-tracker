de932e055378378392afc88125b658912f6dc8ff
Nicolas Payot <nicolas.payot@gmail.com>
2015-10-30 14:43:23 +0100

    test(server): add failing test for GET request with no param

--- api/test/server.test.js
+++ api/test/server.test.js
@@ -1,13 +1,28 @@
 import test from 'tape';
 import httpMocks from 'node-mocks-http';
 import server from '../src/server';
+import workout from '../src/workout';
 
-test('server request', function(t) {
-  const req = httpMocks.createRequest();
+function setup() {
+  workout._sessions = [];
+}
+
+test('server workouts GET request with no param', function(t) {
+  setup();
+
+  workout._sessions = [
+    { id: 1, name: 'workout session 1'},
+    { id: 2, name: 'workout session 2'}
+  ];
+
+  const req = httpMocks.createRequest({
+    method: 'GET',
+    url: '/workouts'
+  });
   const res = httpMocks.createResponse();
   server(req, res);
 
   const body = res._getData();
-  t.equal(body, 'Hello, World!', 'should return "Hello, World!"');
+  t.deepEqual(JSON.parse(body), workout._sessions, 'should return workout sessions list');
   t.end();
 });
